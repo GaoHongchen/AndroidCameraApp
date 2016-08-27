0222c514161f8fb48aec253ed24c1821071bd0dd
GaoHongchen <1656738573@qq.com>
2016-08-27 19:09:36 +0800

    add Orientation Sensor

--- src/com/example/androidcameraapp/MainActivity.java
+++ src/com/example/androidcameraapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidcameraapp;
 
+import com.UserDevice.Sensors.OrientationSensor;
 import com.UserDevice.UserCamera.UserCamera;
 
 import android.app.Activity;
@@ -11,16 +12,27 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private UserCamera mUserCamera;
 	private int nCamType;
+	private int widthCameraPreview;
+	private int heightCameraPreview;
+	
+	//方向传感器相关变量
+	private ImageView imgCircleStatic;
+	private ImageView imgCircle;
+	private int xImgCircle;
+	private int yImgCircle;
+	private OrientationSensor orientationSensor;
 	
 	public MainActivity(){
 		nCamType = 1;//默认后置摄像头
@@ -44,25 +56,14 @@ public class MainActivity extends Activity {
 		manager.getDefaultDisplay().getMetrics(outMetrics);
 		int widthWin = outMetrics.widthPixels;
 		//int heightWin = outMetrics.heightPixels;
+			
+		widthCameraPreview = widthWin;
+		heightCameraPreview = widthWin*4/3;
 		
-		try{
-			nCamType = getIntent().getExtras().getInt("camType");
-		}
-		catch(Exception e){
-		}
+		InitOrientationSensor();
 		
-		if(nCamType == 0){
-			mUserCamera = new UserCamera(this,UserCamera.CameraType.CAMERA_FRONT);	
-		}
-		if(nCamType == 1){
-			mUserCamera = new UserCamera(this,UserCamera.CameraType.CAMERA_BACK);	
-		}
-
-		mUserCamera.SetPreviewSize(widthWin, widthWin/3*4);
-		//set it as the content of our activity.
-		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-		preview.addView(mUserCamera.mPreview);
-
+		InitUserCamera();
+		
 		Button btnCapture = (Button)findViewById(R.id.button_capture);
 		btnCapture.setOnClickListener(new View.OnClickListener() {		
 			@Override
@@ -124,5 +125,72 @@ public class MainActivity extends Activity {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		// TODO Auto-generated method stub
 		return super.onKeyDown(keyCode, event);
-	}	
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		orientationSensor.RegisterListener();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub		
+		super.onStop();
+		
+		orientationSensor.UnRegisterListener();
+	}
+	
+	
+	private void InitOrientationSensor(){
+		imgCircleStatic  = (ImageView)findViewById(R.id.img_circle_static);
+		imgCircle = (ImageView)findViewById(R.id.img_circle);
+		
+		//初始化圆图形的位置
+		ViewGroup.LayoutParams parasImgCircleStatic = imgCircleStatic.getLayoutParams();
+		int diameterCircleStatic = 100;
+		parasImgCircleStatic.width  = diameterCircleStatic;
+		parasImgCircleStatic.height = diameterCircleStatic;
+		imgCircleStatic.setLayoutParams(parasImgCircleStatic);
+		
+		int xImgCircleStatic = (widthCameraPreview-diameterCircleStatic)/2;
+		int yImgCircleStatic = (heightCameraPreview-diameterCircleStatic)/2;
+		imgCircleStatic.setTranslationX(xImgCircleStatic);
+		imgCircleStatic.setTranslationY(yImgCircleStatic);
+			
+		ViewGroup.LayoutParams parasImgCircle = imgCircle.getLayoutParams();
+		int diameterCircle = 90;
+		parasImgCircle.width  = diameterCircle;
+		parasImgCircle.height = diameterCircle;
+		imgCircle.setLayoutParams(parasImgCircle);
+		
+		xImgCircle = (widthCameraPreview-diameterCircle)/2;
+		yImgCircle = (heightCameraPreview-diameterCircle)/2;
+		imgCircle.setTranslationX(xImgCircle);
+		imgCircle.setTranslationY(yImgCircle);	
+		
+		orientationSensor = new OrientationSensor(this,imgCircle,xImgCircle,yImgCircle);
+	}
+	
+	private void InitUserCamera(){
+		try{
+			nCamType = getIntent().getExtras().getInt("camType");
+		}
+		catch(Exception e){
+		}
+		
+		if(nCamType == 0){
+			mUserCamera = new UserCamera(this,UserCamera.CameraType.CAMERA_FRONT);	
+		}
+		if(nCamType == 1){
+			mUserCamera = new UserCamera(this,UserCamera.CameraType.CAMERA_BACK);	
+		}
+
+		mUserCamera.SetPreviewSize(widthCameraPreview, heightCameraPreview);
+		//set it as the content of our activity.
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mUserCamera.mPreview);
+	}
 }
