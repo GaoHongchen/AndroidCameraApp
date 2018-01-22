d00e2a7c826f486c00770a264f36f12215642510
GaoHongchen <1656738573@qq.com>
2018-01-22 17:09:37 +0800

    optimize APP_CPPFLAGS

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,5 +2,5 @@ APP_STL := c++_static #gnustl_static gnustl_shared c++_static
 
 APP_ABI := armeabi-v7a
 
-APP_CPPFLAGS := -frtti -fexceptions -ftree-vectorize -Wno-deprecated -pthread -stdlib=libc++ -std=c++11 -o3\
-				 #-mfloat-abi=softfp -o3 -0fast --ffast-math  -stdlib=libc++ -std=c++0x
+APP_CPPFLAGS := -frtti -fexceptions -ftree-vectorize -Wno-deprecated -pthread -std=c++11 -O3 \
+                -mfloat-abi=softfp -mfpu=neon -Ofast -ffast-math -fno-finite-math-only
